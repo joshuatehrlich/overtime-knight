57610f969a2cc54948f94b4f87e0ee185ba0574f
Joshua Ehrlich <joshuatehrlich@gmail.com>
2023-07-03 19:09:42 -0700

    do a whole bunch of shit fuck you

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,59 @@
 extends CharacterBody2D
 
+const SPEED = 150.0
+const JUMP_VELOCITY = -350.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var sprite_2d : Sprite2D = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	handle_movement(delta)
+	handle_animation()
+
+func handle_movement(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	var direction = Input.get_action_strength("right") - Input.get_action_strength("left")
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func handle_animation():
+	# Determine input direction
+	var direction = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	# Scale character horizontally to make them face the direction of input at all times
+	if direction != 0:
+		sprite_2d["scale"] = Vector2(sign(direction), 1)
+	
+	# Jumping Case
+	if (!is_on_floor()):
+		animation_tree["parameters/conditions/airborne"] = true
+		animation_tree["parameters/airborne/blend_position"] = -sign(velocity.y)
+		animation_tree["parameters/conditions/is_moving"] = false
+		animation_tree["parameters/conditions/idle"] = false
+		return
+	
+	# Grounded Case
+	animation_tree["parameters/conditions/airborne"] = false
+	if (direction) == 0:
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
