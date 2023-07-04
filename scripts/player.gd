6b4790b925fe25eaff19b353d8a3496fdf019642
aemino <aeminodev@gmail.com>
2023-07-04 16:01:37 -0700

    improve movement accel, impl coyote time for jumps

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,48 +1,74 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
+const RUN_SPEED: float = 180.0
+const RUN_ACCEL: float = 2000.0
+const RUN_DECEL: float = 800.0
+const RUN_ACCEL_AIR_FACTOR: float = 0.65
 
-@onready var animation_tree : AnimationTree = $AnimationTree
-@onready var sprite_2d : Sprite2D = $Sprite2D
+const JUMP_SPEED: float = 350.0
+
+const COYOTE_TIME_SECS: float = 0.1
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	handle_movement(delta)
 	handle_animation()
 
-func handle_movement(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_action_strength("right") - Input.get_action_strength("left")
-	if direction != 0:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func handle_movement(delta: float) -> void:
+	handle_movement_gravity(delta)
+	handle_movement_run(delta)
+	handle_movement_jump(delta)
 
 	move_and_slide()
 
+func handle_movement_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func handle_movement_run(delta: float) -> void:
+	var direction := Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	var norm_vel := velocity.x / RUN_SPEED
+	var norm_target_vel := direction
+	var is_accelerating := norm_vel * norm_target_vel >= 0
+	var accel = RUN_ACCEL if is_accelerating else RUN_DECEL
+	var accel_coeff = 1 if is_on_floor() else RUN_ACCEL_AIR_FACTOR
+	
+	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, accel * accel_coeff * delta)
+
+var last_jump_input: float = INF
+var last_grounded: float = INF
+
+func handle_movement_jump(delta: float) -> void:
+	last_jump_input += delta
+	last_grounded += delta
+	
+	if Input.is_action_just_pressed("jump"):
+		last_jump_input = 0.0
+	
+	if is_on_floor():
+		last_grounded = 0.0
+		
+	if (is_on_floor() || last_grounded <= COYOTE_TIME_SECS) && last_jump_input <= COYOTE_TIME_SECS:
+		velocity.y = -JUMP_SPEED
+			
+
 func handle_animation():
 	# Determine input direction
-	var direction = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var direction := Input.get_action_strength("right") - Input.get_action_strength("left")
 	
 	# Scale character horizontally to make them face the direction of input at all times
 	if direction != 0:
 		sprite_2d["scale"] = Vector2(sign(direction), 1)
 	
 	# Jumping Case
-	if (!is_on_floor()):
+	if not is_on_floor():
 		animation_tree["parameters/conditions/airborne"] = true
 		animation_tree["parameters/airborne/blend_position"] = -sign(velocity.y)
 		animation_tree["parameters/conditions/is_moving"] = false
@@ -51,7 +77,7 @@ func handle_animation():
 	
 	# Grounded Case
 	animation_tree["parameters/conditions/airborne"] = false
-	if (direction) == 0:
+	if direction == 0:
 		animation_tree["parameters/conditions/idle"] = true
 		animation_tree["parameters/conditions/is_moving"] = false
 	else:
