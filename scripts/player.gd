124a7d46da8e4abdde6c6e165f3e8885db152214
Ian Lum <ianlum314@gmail.com>
2023-07-04 00:38:41 -0700

    Revert "implement variable jump and fall gravity and set jump height"
    This reverts commit efdc36b1eae2cff305f6cdcd72e49473c93cabbe.

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,28 +9,19 @@ const JUMP_VELOCITY = -350.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Jump implmentation courtesy of: https://www.youtube.com/watch?v=fE8f5-ZHD_k
-var jump_height = 75
-var jump_up_time = 0.3
-var jump_fall_time = 0.2
-
-@onready var jump_velocity : float = ((2.0 * jump_height) / jump_up_time) * -1.0
-@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_up_time * jump_up_time)) * -1.0
-@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_fall_time * jump_fall_time)) * -1.0
 
 func _physics_process(delta):
 	handle_movement(delta)
 	handle_animation()
 
 func handle_movement(delta):
-	if velocity.y < 0.0:
-		velocity.y += jump_gravity * delta
-	else:
-		velocity.y += fall_gravity * delta
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
